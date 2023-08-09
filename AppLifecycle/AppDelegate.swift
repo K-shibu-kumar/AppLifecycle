2f133a8e5f97016ae98a41eeb54bc90bcf2771ef
Dixon A <dixona2023@icloud.com>
2023-08-09 18:44:13 +0530

    Initial Commit

---
+++ AppLifecycle/AppDelegate.swift
@@ -0,0 +1,33 @@
+
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        print(#function)
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+     
+        print(#function)
+        
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        
+        print(#function)
+     
+    }
+
+
+}
---
+++ AppLifecycle/SceneDelegate.swift
@@ -0,0 +1,49 @@
+
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+   
+        print(#function)
+        
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+
+        print(#function)
+        
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+   
+        print(#function)
+        
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+      
+        print(#function)
+        
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+       
+        print(#function)
+        
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+        print(#function)
+        
+    }
+
+
+}
+
